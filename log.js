f07cb52336ed1e5182b728549b67426c1a7a4728
‘haloged’ <aokhfj@outlook.com>
2023-07-14 15:59:21 +0800

    [feet]增加

---
+++ log.js
@@ -0,0 +1,19 @@
+//控制台输出
+
+var version = 'v0.3.8(0038)-dev' //版本号
+console.log(`%c
+H                  H
+H                  H
+H                  H
+HHHHHHHHHHHHHHHHHHHH
+H                  H
+H                  H
+H                  H
+H                  H
+H                  H
+H                  H
+`, "font-weight:bold;color:#6d50f0")
+console.log('欢迎访问haloged studio 官方网站！')
+console.log('感谢haloged studio的所有开发者们!');
+console.log('假装是一个小小的彩蛋~');
+console.log("" + version + " ©️ 2022-2023 haloged studio, All rights reserved.")
