38b5796eb2636d74359bbd69c87b8f58d66e8a71
haloged <101408211+haloged@users.noreply.github.com>
2024-05-19 11:51:47 +0800

    Update log.js

--- log.js
+++ log.js
@@ -1,19 +1,15 @@
 //控制台输出
 
-var version = 'v0.3.8(0038)-dev' //版本号
-console.log(`%c
-H                  H
-H                  H
-H                  H
-HHHHHHHHHHHHHHHHHHHH
-H                  H
-H                  H
-H                  H
-H                  H
-H                  H
-H                  H
-`, "font-weight:bold;color:#6d50f0")
+var version = 'v0.3.9(0045)-dev' //版本号
+console.log(`
+   __ __     __                 __    ______          ___    
+  / // /__ _/ /__  ___ ____ ___/ /   / __/ /___ _____/ (_)__ 
+ / _  / _ `/ / _ \/ _ `/ -_| _  /   _\ \/ __/ // / _  / / _ \
+/_//_/\_,_/_/\___/\_, /\__/\_,_/   /___/\__/\_,_/\_,_/_/\___/
+                 /___/                                       
+`)
 console.log('欢迎访问haloged studio 官方网站！')
 console.log('感谢haloged studio的所有开发者们!');
+console.log('想加入Haloged Studio吗？加入团队请发送邮件至hr@haloged-studio.eu.org(备注来自console~)')
 console.log('假装是一个小小的彩蛋~');
-console.log("" + version + " ©️ 2022-2023 haloged studio, All rights reserved.")
+console.log("" + version + " ©️ 2022-2024 haloged studio, All rights reserved.")
